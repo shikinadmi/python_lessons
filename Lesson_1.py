74b2148b64e4686313cd1c62c94d021a6140c101
Shikin Aleksandr <shikin.a.dmi@icloud.com>
2022-05-26 21:34:06 +0300

    while | break continue

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,6 +1,16 @@
-num = int(input("Enter a number 0-9: "))
-print(type(num))
+# num = int(input("Enter a number 0-9: "))
+# print(type(num))
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
 
-while num <= 10:
-    print(num)
-    num += 1
+i = 0
+
+while True:
+    i += 1
+    if i >= 10:
+        break
+    if i % 2 == 0:
+        continue
+    print(i)
